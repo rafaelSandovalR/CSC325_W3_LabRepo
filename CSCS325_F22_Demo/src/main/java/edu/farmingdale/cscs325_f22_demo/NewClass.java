90781d8b8a9a152552b4e3da335d9e7d1ebed34b
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2022-08-31 10:05:25 -0400

    Refactor main method and add sayHello

--- CSCS325_F22_Demo/src/main/java/edu/farmingdale/cscs325_f22_demo/NewClass.java
+++ CSCS325_F22_Demo/src/main/java/edu/farmingdale/cscs325_f22_demo/NewClass.java
@@ -3,7 +3,12 @@ package edu.farmingdale.cscs325_f22_demo;
 
 public class NewClass {
     
+    private void sayHello(String str){
+        System.out.println(str);
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        NewClass nc = new NewClass();
+        nc.sayHello("Hello World");
     }
 }
