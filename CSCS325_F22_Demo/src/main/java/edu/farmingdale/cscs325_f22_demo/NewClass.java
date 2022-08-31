e4905c456ef3e003ed6131af8d3b592326734383
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2022-08-31 10:28:03 -0400

    Add sayHello with iterator

--- CSCS325_F22_Demo/src/main/java/edu/farmingdale/cscs325_f22_demo/NewClass.java
+++ CSCS325_F22_Demo/src/main/java/edu/farmingdale/cscs325_f22_demo/NewClass.java
@@ -3,12 +3,32 @@ package edu.farmingdale.cscs325_f22_demo;
 
 public class NewClass {
     
-    private void sayHello(String str){
+    private static void sayHello(String str){
         System.out.println(str);
+    }
+    
+    private static void sayHello(String str, int qty){
+       while(qty > 0){
+           System.out.println(str);
+           qty--;
+       }
     }
     
     public static void main(String[] args) {
         NewClass nc = new NewClass();
         nc.sayHello("Hello World");
+        
+        sayHello("Hey", 5);
+        
+    }
+    
+}
+
+class Class2{
+    public Class2(){
+        
+    }
+    public Class2(int x){
+        
     }
 }
