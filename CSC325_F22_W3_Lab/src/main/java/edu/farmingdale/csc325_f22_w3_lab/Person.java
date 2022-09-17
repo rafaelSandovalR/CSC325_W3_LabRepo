29ee96a82abfbabd9da0cba4aad18ac3bdf083af
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2022-09-17 17:27:29 -0400

    Created Person Interface

---
+++ CSC325_F22_W3_Lab/src/main/java/edu/farmingdale/csc325_f22_w3_lab/Person.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package edu.farmingdale.csc325_f22_w3_lab;
+
+/**
+ *
+ * @author Rsand
+ */
+public interface Person {
+    public void setAddress(String s);
+    public String getAddress();
+}
